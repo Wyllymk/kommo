98f1a926ca6ec4fffbcba9508418c1acf9acb927
Wyllymk <wilsonkabatha@gmail.com>
2025-02-19 13:38:30 +0300

    Initial plugin scaffold with Composer and testing

---
+++ Kommo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Kommo
+ * Plugin URI:      https://github.com/WyllyMk/Kommo
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     Kommo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Kommo
+ */
+
+// Your code starts here.
